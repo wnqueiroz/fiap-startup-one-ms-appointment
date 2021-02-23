c576a6ec4b86535f51edc056987f1a35253c653b
William Queiroz <wnqueiroz@outlook.com>
2021-02-23 19:36:53 -0300

    chore: added static ids to appointment_status table

--- src/contants.ts
+++ src/contants.ts
@@ -1,7 +1,6 @@
-// TODO: create static values for this on appointment_status table
 export enum APPOINTMENT_STATUS {
   PENDING = '19091a27-77a6-42a0-915f-c5adffdd601c',
-  FINISHED = 'EC89B1CA-AEA7-487C-923E-73D672D76A45',
+  FINISHED = 'fbb9dc19-6861-4ae7-acec-269b1e768553',
   CANCEL_CUSTOMER = '7bb87638-25f3-4d5c-b87d-7a6147d12695',
   CANCEL_SYSTEM = 'b15b3641-8c46-494c-a501-02c2cbf8f1c1',
 }
