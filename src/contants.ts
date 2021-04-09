5532d519de6c0fcb44d5dc28b8fb927ed57d15d6
marcelomoreno <marcelo.moreno@avalara.com>
2021-04-08 23:05:35 -0300

    feat: added new endpoints

--- src/contants.ts
+++ src/contants.ts
@@ -6,6 +6,7 @@ export enum APPOINTMENT_STATUS {
 }
 
 export enum KAFKA_TOPICS {
+  APPOINTMENTS_UPDATE = 'appointments.update',
   SERVICES_CREATED = 'services.created',
   SERVICES_UPDATED = 'services.updated',
   SERVICES_DELETED = 'services.deleted',
